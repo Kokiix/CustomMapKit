9e4df5d03253fae22b08b8a30ad75a38146154b9
kenneth <kokiidoki@gmail.com>
2026-06-11 06:58:28 -0400

    rename folders

--- Unity/Assets/Plugins/Assembly-CSharp-firstpass/FluffyUnderware/Curvy/Generator/IOnRequestPath.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace FluffyUnderware.Curvy.Generator
-{
-	[Obsolete("IOnRequestPath is an interface with no added value, and will get removed in a future update. Use IOnRequestProcessing or IPathProvider instead")]
-	public interface IOnRequestPath : IOnRequestProcessing, IPathProvider
-	{
-	}
-}
