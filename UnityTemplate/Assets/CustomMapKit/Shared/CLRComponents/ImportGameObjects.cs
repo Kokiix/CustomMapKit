20752dd95a78fa1a427a4a0b74f2499a7971028d
kenneth <kokiidoki@gmail.com>
2026-06-23 08:41:39 -0400

    feat: reorganize file structure of shared, mainly grouping by prefab instead of asset type
    feat: item dispenser prefab
    
    feat: add background fog to teleporter prefab

--- UnityTemplate/Assets/CustomMapKit/Shared/CLRComponents/ImportGameObjects.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[DefaultExecutionOrder(-500)]
-public class ImportGameObjects : MonoBehaviour
-{
-
-}
---
+++ UnityTemplate/Assets/CustomMapKit/Shared/CLRInternals/CustomDispenser.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class CustomDispenser : MonoBehaviour
+{
+    public WeaponType[] ItemsToSpawn;
+}
